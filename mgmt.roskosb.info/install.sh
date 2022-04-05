e23b22755a12bbb1bf02287219407824a69b7689
Brent Roskos <broskos@redhat.com>
2022-04-05 11:11:39 -0400

    working virt workload cluster

--- mgmt.roskosb.info/install.sh
+++ mgmt.roskosb.info/install.sh
@@ -3,3 +3,8 @@ oc apply -k .
 echo "wait 20 minutes for reboots"
 sleep 1200
 oc apply -k .
+oc delete -f 02-lso-localvolume-lvs.yaml
+sleep 3
+oc apply -f 02-lso-localvolume-lvs.yaml
+oc apply -f 03-lso-filesystemclass-default.yaml
+oc get sc 
